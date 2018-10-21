49ae69331ca709e38a418bd3732ae3f8bb6c72b4
Justin Rahardjo <justindra91@gmail.com>
2018-10-21 14:41:18 -0400

    feat(items): added item detail page

--- src/app.ts
+++ src/app.ts
@@ -12,13 +12,13 @@ export class App {
 
     /* tslint:disable max-line-length */
     config.map([
-      { route: ['', 'top'], name: 'top',        moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Top',  settings: { feedType: 'news' } },
-      { route: ['new'],     name: 'new',        moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'New',  settings: { feedType: 'newest' } },
-      { route: ['show'],    name: 'show',       moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Show', settings: { feedType: 'show' } },
-      { route: ['ask'],     name: 'ask',        moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Ask',  settings: { feedType: 'ask' } },
-      { route: ['jobs'],    name: 'jobs',       moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Jobs', settings: { feedType: 'jobs' } },
-      // { route: 'item/:id',  name: 'itemDetail', moduleId: PLATFORM.moduleName('pages/feed'), title: 'jobs' },
-      // { route: 'users/:id', name: 'userDetail', moduleId: PLATFORM.moduleName('pages/feed'), title: 'jobs' },
+      { route: ['', 'top'], name: 'top',          moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Top',  settings: { feedType: 'news' } },
+      { route: 'new',       name: 'new',          moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'New',  settings: { feedType: 'newest' } },
+      { route: 'show',      name: 'show',         moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Show', settings: { feedType: 'show' } },
+      { route: 'ask',       name: 'ask',          moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Ask',  settings: { feedType: 'ask' } },
+      { route: 'jobs',      name: 'jobs',         moduleId: PLATFORM.moduleName('pages/feed'), nav: true, title: 'Jobs', settings: { feedType: 'jobs' } },
+      { route: 'item/:id',  name: 'item-detail',  moduleId: PLATFORM.moduleName('pages/item'), title: 'Item' },
+      { route: 'users/:id', name: 'user-detail',  moduleId: PLATFORM.moduleName('pages/feed'), title: 'User' },
     ]);
     /* tslint:enable max-line-length */
   }
---
+++ src/pages/item.ts
@@ -0,0 +1,13 @@
+import { Item } from './../api/interfaces';
+import { API } from './../api/index';
+export class ItemView {
+  item: Item;
+
+  activate(params) {
+    API.item(params.id)
+      .then((res) => {
+        this.item = res;
+        console.log(res);
+      });
+  }
+}
